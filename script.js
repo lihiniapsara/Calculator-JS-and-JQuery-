7f59eeef62823541d970e29f87bec9a473e12a42
lihiniapsara <apsaralihini11@gmail.com>
2024-10-23 11:29:28 +0530

    2nd

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
@@ -27,4 +28,40 @@ themeToggleBtn.onclick = () => {
     calculator.classList.toggle("dark");
     themeToggleBtn.classList.toggle("active");
     isDark = !isDark;
+};*/
+/*const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+
+buttons.forEach(button => {
+    button.onclick = () => {
+        const { id } = button;
+
+        if (id === "clear") display.innerText = "";
+        else if (id === "backspace") display.innerText = display.innerText.slice(0, -1);
+        else if (id === "equal") {
+            display.innerText = display.innerText ? eval(display.innerText) : "Empty!";
+            if (!display.innerText) setTimeout(() => (display.innerText = ""), 2000);
+        } else display.innerText += id;
+    };
+});
+
+themeToggleBtn.onclick = () => {
+    calculator.classList.toggle("dark");
+    themeToggleBtn.classList.toggle("active");
+};*/
+const display = document.querySelector("#display");
+document.querySelectorAll("button").forEach(button => {
+    button.onclick = () => {
+        if (button.id === "clear") display.innerText = "";
+        else if (button.id === "backspace") display.innerText = display.innerText.slice(0, -1);
+        else if (button.id === "equal") display.innerText = eval(display.innerText) || "Empty!";
+        else display.innerText += button.id;
+    };
+});
+
+document.querySelector(".theme-toggler").onclick = () => {
+    document.querySelector(".calculator").classList.toggle("dark");
+    document.querySelector(".theme-toggler").classList.toggle("active");
 };
